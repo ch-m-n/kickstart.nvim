b7797b937e30751d8c2dc625d7c8de73190f2349
ch-m-n <chaungo330@gmail.com>
2024-07-23 13:15:16 +0700

    init

--- init.lua
+++ init.lua
@@ -882,6 +882,24 @@ require('lazy').setup({
   },
 
   {
+    'romgrk/barbar.nvim',
+    dependencies = {
+      'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
+      'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
+    },
+    init = function()
+      vim.g.barbar_auto_setup = false
+    end,
+    opts = {
+      -- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
+      animation = true,
+      insert_at_start = true,
+      -- …etc.
+    },
+    version = '^1.0.0', -- optional: only update when a new 1.x version is released
+  },
+
+  {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
@@ -923,12 +941,12 @@ require('lazy').setup({
           lualine_z = {},
         },
         tabline = {
-          lualine_a = { 'buffers' },
-          lualine_b = {},
-          lualine_c = {},
-          lualine_x = {},
-          lualine_y = {},
-          lualine_z = { 'tabs' },
+          --lualine_a = { 'buffers' },
+          --lualine_b = {},
+          --lualine_c = {},
+          --lualine_x = {},
+          --lualine_y = {},
+          --lualine_z = { 'tabs' },
         },
         winbar = {},
         inactive_winbar = {},
